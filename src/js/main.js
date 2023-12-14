8b2b28aacde36b2a1f5757ad555e8a5a2f775926
Abdulmunim <amunim729@gmail.com>
2023-12-14 04:10:31 +0300

    added javascript for hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,14 @@
 // Add a click listener to the toggle #nav-toggle
 
-toggle = document.querySelector("#nav-toggle");
+toggle = document.querySelector(".navbar-burger");
 
 if (toggle) {
   toggle.addEventListener("click", function() {
-    document.querySelector("#nav-list").classList.toggle("nav__list--show");
-    document.querySelector("#nav-menu-icon").classList.toggle("hidden");
-    document.querySelector("#nav-close-icon").classList.toggle("hidden");
-    document.querySelector("#nav-toggle").classList.toggle("nav__toggle--close");
-    
+    document.querySelector("#navbar-items").classList.toggle("is-active");
+    document.querySelector("#nav-menu-icon").classList.toggle("is-active");    
   });
 }
 
-const navLanguage = document.querySelector('.nav__language');
-if(navLanguage) {
-const navLanguageText = document.querySelector('.nav__language--text');
-const navLanguageIcon = document.querySelector('.nav__language--icon');
-const navLanguageArrow = document.querySelector('.nav__language--text i');
-const navLanguageList = document.querySelector('.nav__language--list');
-const navLanguageItems = document.querySelectorAll('.nav__language--item');
-const navLanguageItemText = document.querySelectorAll('.nav__language--item span');
-navLanguage.addEventListener('click', () => {
-  navLanguageList.classList.toggle('nav__language--list--show');
-  navLanguageArrow.classList.toggle('rotate');
-});
-navLanguageItems.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    navLanguageText.textContent = navLanguageItemText[index].textContent;
-    navLanguageList.classList.remove('nav__language--list--show');
-    navLanguageArrow.classList.remove('rotate');
-  });
-});
-}
 
 
 let audioEl = document.getElementById("help-audio");
